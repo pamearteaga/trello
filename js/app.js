325b860cfccf8219c39d98e5f435fb7318ae3b43
pamearteaga <pe_arteaga@hotmail.com>
2017-11-07 14:51:55 -0300

    réplica Trello

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+//MOSTRAR INPUT PARA AÑADIR LISTA
 
 var addList = document.getElementById("list");
 
@@ -8,4 +9,48 @@ addList.addEventListener("click", function(){
 	document.getElementById("lista").classList.add("after");
 	document.getElementById("buttons").classList.remove("hidden");
 });
+
+
+// CERRAR INPUT PARA AÑADIR LISTA
+
+
+var closeList = document.getElementById("close");
+
+
+closeList.addEventListener("click", function(){
+
+	document.getElementById("lista").classList.remove("after");
+	document.getElementById("buttons").classList.add("hidden");
+});
+
+
+
+// MOSTRAR INPUT PARA AÑADIR TARJETA
+
+
+var addCard = document.getElementById("card");
+
+
+addCard.addEventListener("click", function(){
+
+
+	document.getElementById("buttons-card").classList.remove("hidden");
+	document.getElementById("card").classList.add("input-card");
+});
+
+
+
+
+var closeCard = document.getElementById("close-card");
+
+
+closeCard.addEventListener("click", function(){
+
+
+	document.getElementById("buttons-card").classList.add("hidden");
+	document.getElementById("card").classList.remove("input-card");
+});
+
+
+
 
