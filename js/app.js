fb26e73fa80ebb7dc73bbdb083f8d9f3995afc58
pamearteaga <pe_arteaga@hotmail.com>
2017-11-07 11:33:55 -0300

    réplica Trello

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+var addList = document.getElementById("list");
+
+
+addList.addEventListener("click", function(){
+
+	document.getElementById("lista").classList.add("after");
+	document.getElementById("buttons").classList.remove("hidden");
+});
+
