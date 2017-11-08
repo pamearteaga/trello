c918678a1ca109d62f97f22a854416316405f1ef
pamearteaga <pe_arteaga@hotmail.com>
2017-11-07 22:28:22 -0300

    réplica Trello

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ addList.addEventListener("click", function(){
 
 var closeList = document.getElementById("close");
 
-
+   
 closeList.addEventListener("click", function(){
 
 	document.getElementById("lista").classList.remove("after");
@@ -25,6 +25,63 @@ closeList.addEventListener("click", function(){
 
 
 
+// BOTON GUARDAR LISTA
+
+var saveList = document.getElementById("save");
+var lista = document.getElementById("lista");
+var main = document.getElementById("principal");
+
+
+saveList.addEventListener("click", function() {
+
+	// variables titulo lista
+    var textList = document.getElementById("list").value;
+    document.getElementById("list").value = "";
+    var pList = document.createElement("p");
+    var contentList = document.createTextNode(textList);
+
+
+    // variable textarera
+    var textArea = document.createElement("textarea");
+    // falta agregar atributo placeholder
+
+
+    // variable botones
+    var divButtons = document.createElement("div");
+    var saveButton = document.createElement("button");
+    var textButton = document.createTextNode("Guardar");
+    var closeButton = document.createElement("span");
+
+    // seccion donde va la nueva lista
+    var divList = document.createElement("div");
+
+  
+    // crear nueva lista  
+    //document.getElementById("save").disabled = false;
+
+    // crear titulo
+    pList.appendChild(contentList);
+    divList.appendChild(pList);
+
+
+    // crear textarea
+    divList.appendChild(textArea);
+
+    // crear botones
+    saveButton.appendChild(textButton);
+    divButtons.appendChild(saveButton);
+    divButtons.appendChild(closeButton);
+    
+    divList.appendChild(divButtons);
+
+    main.insertBefore(divList,lista);
+
+    
+   
+});
+
+
+/*
 // MOSTRAR INPUT PARA AÑADIR TARJETA
 
 
@@ -33,23 +90,22 @@ var addCard = document.getElementById("card");
 
 addCard.addEventListener("click", function(){
 
-
 	document.getElementById("buttons-card").classList.remove("hidden");
 	document.getElementById("card").classList.add("input-card");
 });
 
 
-
+// CERRAR INPUT PARA AÑADIR TARJETA
 
 var closeCard = document.getElementById("close-card");
 
 
 closeCard.addEventListener("click", function(){
 
-
 	document.getElementById("buttons-card").classList.add("hidden");
 	document.getElementById("card").classList.remove("input-card");
 });
+*/
 
 
 
